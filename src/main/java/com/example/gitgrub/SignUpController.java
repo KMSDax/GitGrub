407b4ecee20ee27470f2f6429c6c9d3d5eaec9fa
Jayson Gonzalez <89937039+YsGonzo@users.noreply.github.com>
2023-10-20 18:32:48 -0400

    testing commit

--- src/main/java/com/example/gitgrub/SignUpController.java
+++ src/main/java/com/example/gitgrub/SignUpController.java
@@ -1,4 +1,23 @@
 package com.example.gitgrub;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class SignUpController {
+    @FXML
+    private Button registerButton;
+    @FXML
+    private TextField userField;
+    @FXML
+    private PasswordField confirmPasswordField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private TextField genderField;
+    @FXML
+    private TextField weightField;
+    @FXML
+    private TextField heightField;
 }
