de028230981bd4da86657c9657fddfd43957f382
Jayson Gonzalez <89937039+YsGonzo@users.noreply.github.com>
2023-11-13 22:37:43 -0500

    ShowNutritionLabel inside cookbook

--- src/main/java/com/example/gitgrub/CookbookController.java
+++ src/main/java/com/example/gitgrub/CookbookController.java
@@ -1,5 +1,6 @@
 package com.example.gitgrub;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -19,6 +20,7 @@ import org.jsoup.nodes.Document;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import static com.example.gitgrub.Spoonacular.fetchNutritionLabel;
 import static com.example.gitgrub.Spoonacular.fetchRecipeInformation;
 
 public class CookbookController extends MainApplication implements Initializable {
@@ -27,11 +29,13 @@ public class CookbookController extends MainApplication implements Initializable
     @FXML
     public Hyperlink sourceLink1, sourceLink2, sourceLink3, sourceLink4;
     @FXML
-    public WebView descriptionPane1, descriptionPane2, descriptionPane3, descriptionPane4;
+    public WebView descriptionPane1, descriptionPane2, descriptionPane3, descriptionPane4, webView;
+    @FXML
+    public Pane nutritionLabelPane;
     @FXML
     public ImageView imageView1, imageView2, imageView3, imageView4;
     @FXML
-    public Button back,next;
+    public Button back,next, nutritionButton1, nutritionButton2, nutritionButton3, nutritionButton4;
     public TextField search;
     private int currentIndex = 0;
     @Override
@@ -73,6 +77,44 @@ public class CookbookController extends MainApplication implements Initializable
             }
         }
     }
+
+    // fetchNutritionLabel looping through recipeId to correspond with the displaying Recipes (Using nutritionButtons)
+    public void showRecipeNutritionLabel(ActionEvent event) {
+        webView = new WebView();
+        Button nutritionButton = (Button) event.getSource();
+        int recipeId;
+        switch (nutritionButton.getId()) {
+            case "nutritionButton1":
+                recipeId = currentIndex + 1;
+                break;
+            case "nutritionButton2":
+                recipeId = currentIndex + 2;
+                break;
+            case "nutritionButton3":
+                recipeId = currentIndex + 3;
+                break;
+            case "nutritionButton4":
+                recipeId = currentIndex + 4;
+                break;
+            default:
+                recipeId = -1; // Handle any other cases as needed
+        }
+
+        if (recipeId != -1) {
+            // Fetch the nutrition label using the recipeId
+            String nutritionLabelContent = fetchNutritionLabel(recipeId);
+            if (nutritionLabelContent != null) {
+                webView.getEngine().loadContent(nutritionLabelContent);
+
+                webView.setPrefSize(nutritionLabelPane.getWidth() - 10, nutritionLabelPane.getHeight() - 10);
+                webView.setMaxSize(nutritionLabelPane.getWidth(), nutritionLabelPane.getHeight());
+                webView.setTranslateX(10);
+                webView.setTranslateY(10);
+                nutritionLabelPane.getChildren().add(webView);
+            }
+        }
+    }
+
     private Label getRecipeLabel(int index) {
         switch (index) {
             case 1:
