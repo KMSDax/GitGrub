c82d7e6975674b27f8d17bc15423ec5c7e6a1f24
Hakim-0928 <145039850+Hakim-0928@users.noreply.github.com>
2023-11-21 19:46:29 -0500

    fixed calorie counter , added Meal, MealPlanner, MealPlannerController, and Mealpalnnerfxml to get the users total nutrition

--- src/main/java/com/example/gitgrub/CalorieCountController.java
+++ src/main/java/com/example/gitgrub/CalorieCountController.java
@@ -1,129 +1,48 @@
 package com.example.gitgrub;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.TextField;
-import javax.swing.JOptionPane;
-import java.net.URL;
-import java.util.ResourceBundle;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
 
-public class CalorieCountController implements Initializable {
+public class CalorieCountController {
 
     @FXML
     private TextField mealNameField;
-    @FXML
-    private TextField foodItemField;
-    @FXML
-    private TextField portionSizeField;
-    @FXML
-    private TextField ageField;
-    @FXML
-    private TextField heightField;
-    @FXML
-    private TextField weightField;
-    @FXML
-    private ListView<Meal> itemsListView;
-    @FXML
-    private Button addButton;
-    @FXML
-    private Button removeButton;
-    @FXML
-    private Button calculateButton;
-    @FXML
-    private Label caloriesResLabel;
 
-    private ObservableList<Meal> listItems = FXCollections.observableArrayList();
+    @FXML
+    private TextField caloriesField;
 
-    private void addButtonAction() {
-        addButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent t) {
-                // Check if all fields are filled
-                if (mealNameField.getText().isEmpty() || foodItemField.getText().isEmpty() ||
-                        portionSizeField.getText().isEmpty() || ageField.getText().isEmpty() ||
-                        heightField.getText().isEmpty() || weightField.getText().isEmpty()) {
-                    JOptionPane.showMessageDialog(null, "All fields are mandatory!");
-                    return;
-                }
+    @FXML
+    private VBox mealSummaryBox;
 
-                // Parse input values
-                String mealName = mealNameField.getText().trim();
-                String foodName = foodItemField.getText().trim();
-                int size = Integer.parseInt(portionSizeField.getText().trim());
-                int age = Integer.parseInt(ageField.getText().trim());
-                double height = Double.parseDouble(heightField.getText().trim());
-                double weight = Double.parseDouble(weightField.getText().trim());
+    @FXML
+    private Label totalCaloriesLabel;
 
-                // Create a new Meal object and add it to the list
-                listItems.add(new Meal(mealName, foodName, size));
+    public void addMeal() {
+        String mealName = mealNameField.getText();
+        String caloriesText = caloriesField.getText();
 
-                // Clear input fields
-                mealNameField.clear();
-                foodItemField.clear();
-                portionSizeField.clear();
-                ageField.clear();
-                heightField.clear();
-                weightField.clear();
+        if (!mealName.isEmpty() && !caloriesText.isEmpty()) {
+            try {
+                int calories = Integer.parseInt(caloriesText);
 
-                itemsListView.setItems(listItems);
+                // Update the meal summary
+                Label mealLabel = new Label(mealName + ": " + calories + " calories");
+                mealSummaryBox.getChildren().add(mealLabel);
+
+                // Update the total calories label
+                updateTotalCalories(calories);
+            } catch (NumberFormatException e) {
+                // Handle invalid calorie input
+                System.out.println("Invalid calories input");
             }
-        });
+        }
     }
 
-    private void removeButtonAction() {
-        removeButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent t) {
-                if (itemsListView.getSelectionModel().isEmpty()) {
-                    JOptionPane.showMessageDialog(null, "Please select at least 1 item from the list!");
-                    return;
-                }
-                itemsListView.getItems().removeAll(itemsListView.getSelectionModel().getSelectedItems());
-            }
-        });
-    }
-
-    private void calculateButtonAction() {
-        calculateButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent t) {
-                if (itemsListView.getItems().isEmpty() || ageField.getText().isEmpty() ||
-                        heightField.getText().isEmpty() || weightField.getText().isEmpty()) {
-                    JOptionPane.showMessageDialog(null, "All fields are mandatory!");
-                    return;
-                }
-
-                // Parse age, height, and weight
-                int age = Integer.parseInt(ageField.getText().trim());
-                double height = Double.parseDouble(heightField.getText().trim());
-                double weight = Double.parseDouble(weightField.getText().trim());
-
-                // Calculate BMR using the Mifflin-St Jeor equation
-                double bmr = 10 * weight + 6.25 * height - 5 * age + 5;
-
-                // Calculate total daily calories based on activity factor (assuming sedentary activity)
-                double activityFactor = 1.2;
-                int totalCalories = (int) (bmr * activityFactor);
-
-                // Update the calories label
-                caloriesResLabel.setText("Calories: " + totalCalories);
-            }
-        });
-    }
-
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        itemsListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-        addButtonAction();
-        removeButtonAction();
-        calculateButtonAction();
+    private void updateTotalCalories(int calories) {
+        String totalCaloriesText = totalCaloriesLabel.getText();
+        int currentTotalCalories = Integer.parseInt(totalCaloriesText.split(":")[1].trim());
+        int newTotalCalories = currentTotalCalories + calories;
+        totalCaloriesLabel.setText("Total Calories: " + newTotalCalories);
     }
 }
