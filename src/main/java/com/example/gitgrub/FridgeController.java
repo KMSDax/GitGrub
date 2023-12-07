aa304f23cb8d166465a495af7d202917d26b9038
StrugglingProgrammer44 <99507565+StrugglingProgrammer44@users.noreply.github.com>
2023-12-07 09:35:32 -0500

    Fixed error

--- src/main/java/com/example/gitgrub/FridgeController.java
+++ src/main/java/com/example/gitgrub/FridgeController.java
@@ -25,7 +25,7 @@ public class FridgeController extends MainApplication implements Initializable {
     public ListView<String> searchListView;
     public TextField searchField;
     public static FridgeManager fridgeManager = new FridgeManager();
-    public static ListView<FridgeItem> fridgeContentsListView;
+    public ListView<FridgeItem> fridgeContentsListView;
 
     public static void main(String[] args) {
         launch(args);
@@ -45,6 +45,41 @@ public class FridgeController extends MainApplication implements Initializable {
         root.setSpacing(10);
 
         Label titleLabel = new Label("Fridge Contents");
+        //ListView<FridgeItem> listView = new ListView<>();
+       // listView.setPrefSize(300, 200);
+
+//        Button addButton = new Button("Add Item");
+//        addButton.setOnAction(e -> {
+//            // Show a dialog to add an item to the fridge
+//            TextInputDialog dialog = new TextInputDialog();
+//            dialog.setTitle("Add Item");
+//            dialog.setHeaderText(null);
+//            dialog.setContentText("Enter item name:");
+//            Optional<String> result = dialog.showAndWait();
+//            result.ifPresent(itemName -> {
+//                // Add the item to the fridge with today's date as expiration
+//                fridgeManager.addItemToFridge(itemName, LocalDate.now());
+//                updateListView(fridgeContentsListView, fridgeManager.getFridgeContents());
+//            });
+     //   });
+
+//        Button removeButton = new Button("Remove Item");
+//        removeButton.setOnAction(e -> {
+//            // Remove the selected item from the fridge
+//            FridgeItem selectedItem = fridgeContentsListView.getSelectionModel().getSelectedItem();
+//            if (selectedItem != null) {
+//                fridgeManager.removeItemFromFridge(selectedItem);
+//                updateListView(fridgeContentsListView, fridgeManager.getFridgeContents());
+//            }
+//        });
+
+//        Button checkButton = new Button("Check Expiration");
+//        checkButton.setOnAction(e -> {
+//            // Check and notify about items close to expiration
+//            fridgeManager.checkFridgeContents();
+//        });
+
+      //  root.getChildren().addAll(titleLabel, fridgeContentsListView, checkButton);
 
         // Set up the JavaFX scene
         Scene scene = new Scene(root, 320, 350);
@@ -53,6 +88,10 @@ public class FridgeController extends MainApplication implements Initializable {
         primaryStage.show();
     }
 
+//    private void updateListView(ListView<FridgeItem> listView, ArrayList<FridgeItem> fridgeContents) {
+//        listView.getItems().clear();
+//        fridgeContents.forEach((item) -> listView.getItems().add(item));
+//    }
 
     public void removeItemAction(ActionEvent actionEvent) {
         fridgeManager.removeItemFromFridge(fridgeContentsListView.getSelectionModel().getSelectedItem());
