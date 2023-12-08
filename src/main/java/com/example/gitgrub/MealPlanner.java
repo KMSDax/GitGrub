7949f1c76f6208b190d9c147037ade3fe7b1a96b
Hakim-0928 <145039850+Hakim-0928@users.noreply.github.com>
2023-12-08 17:33:43 -0500

    fixed MealPlanner and MealPlannerController Removed the displayMeals() method, as it was printing to the console Changed the calculateTotalNutrition() method to update instance variables (totalCalories, totalProtein, totalCarbs, totalFat) instead of printing to the console. Updated the getTotalCalories(), getTotalProtein(), getTotalCarbs(), and getTotalFat() methods to return the calculated totals as strings.

--- src/main/java/com/example/gitgrub/MealPlanner.java
+++ src/main/java/com/example/gitgrub/MealPlanner.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 
 public class MealPlanner {
     private ArrayList<Meal> meals = new ArrayList<>();
+    private int totalCalories = 0;
+    private double totalProtein = 0.0;
+    private double totalCarbs = 0.0;
+    private double totalFat = 0.0;
 
     public void addMeal(Meal meal) {
         meals.add(meal);
@@ -13,19 +17,11 @@ public class MealPlanner {
         return meals;
     }
 
-    public void displayMeals() {
-        System.out.println("Your Meals:");
-        for (Meal meal : meals) {
-            System.out.println(meal);
-        }
-        System.out.println();
-    }
-
     public void calculateTotalNutrition() {
-        int totalCalories = 0;
-        double totalProtein = 0.0;
-        double totalCarbs = 0.0;
-        double totalFat = 0.0;
+        totalCalories = 0;
+        totalProtein = 0.0;
+        totalCarbs = 0.0;
+        totalFat = 0.0;
 
         for (Meal meal : meals) {
             totalCalories += meal.getCalories();
@@ -33,27 +29,21 @@ public class MealPlanner {
             totalCarbs += meal.getCarbs();
             totalFat += meal.getFat();
         }
-
-        System.out.println("Total Nutrition for the Day:");
-        System.out.println("Total Calories: " + totalCalories);
-        System.out.println("Total Protein: " + totalProtein + " grams");
-        System.out.println("Total Carbs: " + totalCarbs + " grams");
-        System.out.println("Total Fat: " + totalFat + " grams\n");
     }
 
     public String getTotalCalories() {
-        return null;
+        return String.valueOf(totalCalories);
     }
 
     public String getTotalProtein() {
-        return null;
+        return String.valueOf(totalProtein);
     }
 
     public String getTotalCarbs() {
-        return null;
+        return String.valueOf(totalCarbs);
     }
 
     public String getTotalFat() {
-        return null;
+        return String.valueOf(totalFat);
     }
 }
